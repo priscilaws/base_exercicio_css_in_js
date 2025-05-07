d05d101f406811f235acdcbd570d206da267cff3
Priscila <pri.wagner.silva@gmail.com>
2025-05-07 12:12:45 +0100

    added global styles

--- src/styles.ts
+++ src/styles.ts
@@ -1,9 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+* {
 :root {
   --cor-principal: #a7727d;
   --cor-secundaria: #f9f5e7;
 }
 
-* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
@@ -25,4 +28,6 @@ body {
   .container {
     max-width: 80%;
   }
-}
+}`
+
+export default EstiloGlobal
